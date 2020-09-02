8e06a959337fba6ae0752f838070b43b3a665987
Stefan Krstikj <st.krstic@gmail.com>
2020-09-03 01:48:03 +0200

    fixed some concurrency issues

--- src/commands.js
+++ src/commands.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 const CoinGecko = require('coingecko-api');
-const { printTable, printChart } = require('./output');
-
-
+const readline = require('readline');
+const { printTable, printChart } = require('./print');
+const config = require('./config.json');
+const { callbackify } = require('util');
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
 
 const CoinGeckoClient = new CoinGecko();
 
@@ -11,39 +14,97 @@ var ping = async () => {
     console.log(data.data['gecko_says'])
 };
 
-var coinsAll = async (n) => {
+var coinsAll = (n, watch, activeCoin = 0) => {
+    if (watch !== undefined && watch < 30) {
+        console.error("error: required option '-w, --watch [seconds]' needs to be greater than 30 seconds!")
+        // return
+    }
+
+    getCoins().then(data => {
+        if (!_isResponseSuccess(data)) {
+            console.log(data.data.error)
+            return;
+        }
+
+        printTable(data.data, watch, activeCoin)
+        
+        process.stdin.on('keypress', (str, key) => {
+            if (key.ctrl && key.name === 'c') {
+                process.exit();
+            } else {
+                switch (key.name) {
+                    case ('up'):
+                        activeCoin == 0 ? activeCoin : activeCoin--
+                        printTable(data.data, watch, activeCoin)
+                        break
+
+                    case ('down'):
+                        ((n !== undefined && activeCoin == n - 1) || (n === undefined && activeCoin == 9)) ? activeCoin : activeCoin++
+                        printTable(data.data, watch, activeCoin)
+                        break
+
+                    case ('return'):
+                        process.stdin.removeAllListeners('keypress')
+                        fetchMarketChartRange(data.data[activeCoin].id, 1, watch)
+                        watch = undefined;
+
+                    default:
+                    // do nothing
+                }
+            }
+        });
+
+        if (watch !== undefined) {
+            new Promise(resolve => {
+                setTimeout(resolve, watch * 1000)
+            }).then(() => coinsAll(n, watch, activeCoin));
+        }
+        else{
+            // process.exit();
+        }
+    })
+};
+
+var getCoins = async (n = 10) => {
     let data = await CoinGeckoClient.coins.all({
         per_page: n ? n : 10
     });
-
-    if (!_isResponseSuccess(data)) {
-        console.log(data.data.error)
-        return;
-    }
-    printTable(data.data)
-};
-
-var coinsFetch = async (coinId) => {
-    let data = await CoinGeckoClient.coins.fetch(coinId, {});
-
-    if (!_isResponseSuccess(data)) {
-        console.log(data.data.error)
-        return;
-    }
+    return data
 }
 
-var fetchMarketChartRange = async (coinId, days = 1) => {
+// var coinsFetch = async (coinId) => {
+//     let data = await CoinGeckoClient.coins.fetch(coinId, {});
+
+//     if (!_isResponseSuccess(data)) {
+//         console.log(data.data.error)
+//         return;
+//     }
+// }
+
+var fetchMarketChartRange = async (coinId, days = 1, watch) => {
     const data = await CoinGeckoClient.coins.fetchMarketChart(coinId, {
         days: days
     })
-    printChart(data.data.prices, coinId)
-}
+    printChart(data.data.prices, coinId, watch)
+    if (watch === undefined)
+        process.exit();
 
+    process.stdin.on('keypress', (str, key) => {
+        if (key.ctrl && key.name === 'c') {
+            process.exit();
+        } else {
+            if(key.name == 'return'){
+                process.stdin.removeAllListeners('keypress')
+                coinsAll(10, watch)
+            }
+        }
+    })
+}
 
 var _isResponseSuccess = function (data) {
     return data.success
 }
 
 module.exports = {
-    ping, coinsAll, coinsFetch, fetchMarketChartRange
+    ping, coinsAll, fetchMarketChartRange
 };
---
+++ src/print.js
@@ -0,0 +1,65 @@
+const Table = require('cli-table');
+const chalk = require('chalk');
+const { formatCurrency } = require('./formatter');
+const config = require('./config.json');
+const babar = require('babar');
+
+var printTable = function (data, watch, activeCoin = 0, clear = false) {
+    const table = new Table({
+        head: [
+            chalk[config.colors.table_head]('Name'),
+            chalk[config.colors.table_head]('Market Cap'),
+            chalk[config.colors.table_head]('Price Change'),
+            chalk[config.colors.table_head]('Current Price'),
+            chalk[config.colors.table_head]('24h High'),
+            chalk[config.colors.table_head]('24h Low')
+        ]
+    });
+    var counter = 0;
+    data.map(it => {
+        table.push([
+            (counter === activeCoin && watch !== undefined) ? chalk[config.colors.table_active_coin].black(it.localization[config.global.localization]) : it.localization[config.global.localization],
+            formatCurrency(it.market_data.market_cap[config.global.currency]),
+            it.market_data.price_change_percentage_24h < 0 ? chalk[config.colors.table_negative](it.market_data.price_change_percentage_24h) : chalk[config.colors.table_positive](it.market_data.price_change_percentage_24h),
+            '$' + (it.market_data.current_price[config.global.currency]),
+            '$' + (it.market_data.high_24h[config.global.currency]),
+            '$' + (it.market_data.low_24h[config.global.currency])
+        ])
+        counter++
+    })
+    if (watch !== undefined)
+        console.clear()
+
+    printTime()
+    console.log(table.toString())
+}
+
+var printChart = ((data, coinId, watch) => {
+    if(watch !== undefined)
+        console.clear()
+    console.log(babar(data, {
+        color: config.colors.chart,
+        height: 10,
+        caption: coinId
+    }));
+    if(watch !== undefined)
+        console.log(' ' + chalk[config.colors.table_active_coin].black('Return'))
+})
+
+var printTime = (() => {
+    var today = new Date();
+    var year = today.getFullYear()
+    var month = today.getMonth() < 10 ? '0' + today.getMonth() : today.getMonth()
+    var day = today.getDay() < 10 ? '0' + today.getDay() : today.getDay()
+    var hours = today.getHours() < 10 ? '0' + today.getHours() : today.getHours()
+    var minutes = today.getMinutes() < 10 ? '0' + today.getMinutes() : today.getMinutes()
+    var seconds = today.getSeconds() < 10 ? '0' + today.getSeconds() : today.getSeconds()
+    console.log('')
+    var timestamp = year + '-' + month + '-' + day + ' ' + hours + ":" + minutes + ":" + seconds
+    console.log(chalk.bold(' Last update:'), timestamp)
+})
+
+
+module.exports = {
+    printTable, printChart
+};
