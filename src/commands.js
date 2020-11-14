179ec9f17f8aad8718fd4e3a5455186a33314645
Stefan Krstikj <st.krstic@gmail.com>
2020-11-14 13:56:28 +0100

    fixed infinite loop bug

--- src/commands.js
+++ src/commands.js
@@ -6,6 +6,7 @@ readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
 const CoinGeckoClient = new CoinGecko();
+var viewingChartData = false
 
 var ping = async () => {
     const data = await CoinGeckoClient.ping();
@@ -17,56 +18,70 @@ var coinsAll = (n, watch, activeCoin = 0) => {
         console.error("error: required option '-w, --watch [seconds]' needs to be greater than 30 seconds!")
         return
     }
-    getCoins(n).then(data => {
-        if (!_isResponseSuccess(data)) {
-            console.log(data.data.error)
-            return;
-        }
-        if (watch !== undefined) {
-            // interactive
-            var timestamp = getTime()
-            var cancelPromise = false
-            printTable(data.data, watch, activeCoin, timestamp, true)
-            process.stdin.on('keypress', (str, key) => {
-                if (key.ctrl && key.name === 'c') {
-                    process.exit();
-                } else {
-                    switch (key.name) {
-                        case ('up'):
-                            if (activeCoin !== 0) activeCoin--;
-                            printTable(data.data, watch, activeCoin, timestamp, true)
-                            return
-
-                        case ('down'):
-                            if (!((n !== undefined && activeCoin === n - 1) || (n === undefined && activeCoin === 9))) activeCoin++;
-                            printTable(data.data, watch, activeCoin, timestamp, true)
-                            return
-
-                        case ('return'):
-                            process.stdin.removeAllListeners('keypress')
-                            fetchMarketChartRange(data.data[activeCoin].id, 1, watch)
-                            cancelPromise = true
-                            return
-
-                        default:
-                        // do nothing
-                    }
-                }
-            })
-            new Promise(resolve => {
-                setTimeout(resolve, watch * 1000)
-            }).then(() => {
-                process.stdin.removeAllListeners('keypress')
-                if (cancelPromise)
-                    return
-                coinsAll(n, watch, activeCoin)
-            });
-
-        } else {
+    if (watch !== undefined) {
+        // interactive
+        interactive(n, watch, activeCoin)
+    } else {
+        getCoins(n).then(data => {
+            if (!_isResponseSuccess(data)) {
+                console.log(data.data.error)
+                return;
+            }
             printTime()
             printTable(data.data, watch, activeCoin)
-        }
-    });
+        })
+    }
+
+}
+
+function interactive(n, watch, activeCoin) {
+    getCoins(n).then(data => {
+        var timestamp = getTime()
+        if(!viewingChartData)
+            printTable(data.data, watch, activeCoin, timestamp, true)
+        process.stdin.on('keypress', (str, key) => {
+            if (key.ctrl && key.name === 'c') {
+                process.exit();
+            } else {
+                switch (key.name) {
+                    case ('up'):
+                        if(!viewingChartData){
+                            if (activeCoin !== 0) activeCoin--;
+                            printTable(data.data, watch, activeCoin, timestamp, true)
+                        }
+                        return
+
+                    case ('down'):
+                        if(!viewingChartData){
+                            if (!((n !== undefined && activeCoin === n - 1) || (n === undefined && activeCoin === 9))) activeCoin++;
+                            printTable(data.data, watch, activeCoin, timestamp, true)
+                        }
+                        return
+
+                    case ('return'):
+                        if(viewingChartData){
+                            viewingChartData = false
+                            printTable(data.data, watch, activeCoin, timestamp, true)
+                        }else{
+                            // process.stdin.removeAllListeners('keypress')
+                            viewingChartData = true
+                            fetchMarketChartRange(data.data[activeCoin].id, 1, watch)
+                        }
+                        return
+
+                    default:
+                        return
+                    // do nothing
+                }
+            }
+        })
+        new Promise(resolve => {
+            setTimeout(resolve, watch * 1000)
+        }).then(() => {
+            process.stdin.removeAllListeners('keypress')
+            interactive(n, watch, activeCoin)
+        });
+    })
 }
 
 var getCoins = async (n = 10) => {
@@ -81,7 +96,6 @@ var fetchMarketChartRange = async (coinId, days = 1, watch) => {
         days: days
     })
     printChart(data.data.prices, coinId, watch)
-    process.exit();
 }
 
 var _isResponseSuccess = function (data) {
